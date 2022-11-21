63073c3e653943830de815e04b0b1a360f634d00
Jeremy-kr <wjddlr905@gmail.com>
2022-11-21 12:35:43 +0900

    init prototype

--- js/firebase.js
+++ js/firebase.js
@@ -1,21 +1,25 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js';
 
-// 아래 데이터는 본인의 Firebase 프로젝트 설정에서 확인할 수 있습니다.
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBLC85NBtPaLOoWyBoSKwexacA6bC4qZX0",
-  authDomain: "privatefanbook.firebaseapp.com",
-  projectId: "privatefanbook",
-  storageBucket: "privatefanbook.appspot.com",
-  messagingSenderId: "691166733828",
-  appId: "1:691166733828:web:7fb27f2b27fb95304fb751",
+  apiKey: 'AIzaSyAaUf9CaLX6rxXJk2GfizWXdjgbPyJAs4k',
+  authDomain: 'tmidol.firebaseapp.com',
+  projectId: 'tmidol',
+  storageBucket: 'tmidol.appspot.com',
+  messagingSenderId: '887608608292',
+  appId: '1:887608608292:web:8af3db4fbd137bbdaafab1',
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const dbService = getFirestore(app);
-export const authService = getAuth(app);
-export const storageService = getStorage(app);
+const app = initializeApp(firebaseConfig);
+const authService = getAuth(app);
+const dbService = getFirestore(app);
+const storageService = getStorage(app);
+
+export { app, authService, dbService, storageService };
